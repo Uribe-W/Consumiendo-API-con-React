0cc768246a3cc5412e6b601754eefbb0f2e0cdfc
wilson uribe <wuribe@pulsar.com.do>
2023-02-14 08:31:50 -0500

    consumiendo una api de prueba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class UserList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ todos: data });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Lista de tareas</h1>
+        <ul className="todo-list">
+          {this.state.todos.map(todo => (
+            <li key={todo.id}>
+              <div className="todo-item">
+                <div className="todo-id">{todo.id}</div>
+                <div className="todo-title">{todo.title}</div>
+                <div className="todo-status">{todo.completed ? 'Completado' : 'Pendiente'}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default UserList;
